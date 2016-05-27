e53e0383b2c6a3f1bccc6b5b1f77f3d8c063c8d3
tapingham <tapingham@gmail.com>
2016-05-27 13:22:36 -0400

    Update Actor.java

--- src/Actor.java
+++ src/Actor.java
@@ -1,1 +1,11 @@
+/**this class will create a character with 
+ * the capability to be displayed to the screen
+ * as well as keep track of the weapon currently equipped
+ * 
+ * @author Troy Passingham
+ * @version May 27 2016
+ *
+ */
+public class Actor {
 
+}

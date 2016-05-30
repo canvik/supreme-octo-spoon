2e3a30fdf233a30be854c1f024c5d5592ad0f836
canvik <guyattachedtolamp@gmail.com>
2016-05-30 13:14:31 -0400

    Create Weapon.java

---
+++ src/Weapon.java
@@ -0,0 +1,22 @@
+/**This object holds the values for the player/enemy weapon, and handles hit detection
+ * 
+ * @author Quinn Bloomfield
+ * @version May 30th 2016
+ *
+ */
+import java.awt.*;
+
+public class Weapon {
+	//Declare and instantiate variables
+	private int currentDamageDealt;
+	private int x, y;
+	private Rectangle currentHitBox;
+	private int range;
+	private boolean inUse = false;
+	private String currentWeapon;
+	
+	//CONSTRUCTOR
+	public Weapon(String weaponName){
+		
+	}//End of constructor
+}//End of class
